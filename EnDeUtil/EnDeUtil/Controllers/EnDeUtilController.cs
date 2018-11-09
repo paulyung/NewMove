22ce5bc936d226bd65f64ede87fe85620685ec78
Paul Yung <paulyung@live.com>
2018-11-09 13:56:18 -0800

    initial

---
+++ EnDeUtil/EnDeUtil/Controllers/EnDeUtilController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EnDeUtil.Controllers
+{
+    //[Route("api/[controller]")]
+    //[Route("EnDeUtil/")]
+    [Route("/")]
+    [ApiController]
+    public class EnDeUtilController : ControllerBase
+    {
+        static string hash = "GoingToTheCloud";
+
+        [HttpGet]
+        [Route("Encrypt")]
+        public string Encrypt(string es)
+        {
+
+            byte[] data = UTF8Encoding.UTF8.GetBytes(es);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider triDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = triDes.CreateEncryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    return Convert.ToBase64String(results, 0, results.Length);
+                }
+
+            }
+        }
+
+        [HttpGet]
+        [Route("Decrypt")]
+        public string Decrypt(string ds)
+        {
+
+            byte[] data = Convert.FromBase64String(ds);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider triDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = triDes.CreateDecryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    return UTF8Encoding.UTF8.GetString(results);
+                }
+
+            }
+        }
+
+    }
+}
